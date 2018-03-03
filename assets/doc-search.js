c7d76914459627b92ffea1a22a46cea8edd6a0b1
azu <azu@users.noreply.github.com>
2018-03-03 16:45:17 +0900

    fix: does not work after moving page

--- assets/doc-search.js
+++ assets/doc-search.js
@@ -1,11 +1,19 @@
 require(["gitbook"], function(gitbook) {
-    gitbook.events.bind("start", function(e, config) {
-        var cfg = config.docSearch;
+    var pluginsConfig = {};
+    var initDocSearch = function() {
+        var cfg = pluginsConfig.docSearch;
         docsearch({
             apiKey: cfg.apiKey,
             indexName: cfg.index,
             inputSelector: '#book-doc-search-input',
             debug: false
         });
+    }
+    gitbook.events.bind("start", function(e, config) {
+        pluginsConfig = config;
+        initDocSearch();
+    });
+    gitbook.events.bind("page.change", function() {
+        initDocSearch();
     });
 });
